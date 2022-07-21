ed64da64795bbc4f8a71ad5454f6994eedc8d19d
Swathi Parkibanda <Swathi.Parkibanda@Benjaminmoore.com>
2022-07-21 15:57:47 +0530

    Mystring class

--- src/Mystring.java
+++ src/Mystring.java
@@ -1,11 +1,13 @@
-
 public class Mystring {
-
-  String a="a boy Has a hat";
-  
-
-	
-
-	
-
+    public static void main (String args[]){
+    String a= "A boy Has a hat";
+    System.out.println(a.length());
+    System.out.println(a.toUpperCase());
+    System.out.println(a.toLowerCase());
+    System.out.println(a.trim());
+    System.out.println(a.substring(0,1));
+    System.out.println(a.indexOf('H'));
+    }
 }
+
+
